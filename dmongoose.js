f53a6ac6148af566befb74689c918a4ccd65667d
riddhi parmar <riddhi.parmar@kevit.io>
2019-03-06 17:28:21 +0530

    Add DELETE /Udata/:id route

---
+++ dmongoose.js
@@ -0,0 +1,22 @@
+const {ObjectId} =require('mongodb')
+const {mongoose} =require('./server/db/datacon');
+const {ud} =require('./server/model/umodel')
+
+  var id = '5c7f8ef9a42948583f192b04'
+
+    ud.findOneAndRemove({_id: id}).then((user)=>
+    {
+            console.log(user);
+    })
+    
+    // ud.findByIdAndRemove(id).then((user)=>
+    // {
+        
+    //     console.log('User data by ID:',user)
+    
+    // })
+
+    ud.remove({}).then((result)=>
+    {
+            console.log(result)
+    })
